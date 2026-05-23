5e38c3d8c6ec712c10342669d7c07efd7af1bfcd
Moyahabo Thabang <thabangrantoka@gmail.com>
2026-05-23 07:09:49 +0200

    Integrate Email JS for contact form functionality
    Updated contact form submission to use EmailJS for sending messages.

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
 const menuIcon = document.querySelector('#menu-icon');
 const navbar = document.querySelector('.navbar');
 
+
+    emailjs.init("NEAuWdVyLiHRYek6G");
+
+    document.getElementById("contactForm")
+    .addEventListener("submit", function(event) {
+
+        event.preventDefault();
+
+        emailjs.send("service_cj2c2pb", "template_yvheqsv", {
+            from_name: document.getElementById("name").value,
+            from_email: document.getElementById("email").value,
+            phone: document.getElementById("phone").value,
+            subject: document.getElementById("subject").value,
+            message: document.getElementById("message").value
+        })
+        .then(function() {
+            alert("Message sent successfully!");
+            document.getElementById("contactForm").reset();
+        })
+        .catch(function(error) {
+            alert("Failed to send message.");
+            console.log(error);
+        });
+
+    });
+
+
 menuIcon.onclick = () => {
     navbar.classList.toggle('active');
 };
@@ -26,13 +53,3 @@ window.onscroll = () => {
     });
 };
 
-
-const form = document.getElementById('contactForm');
-
-form.addEventListener('submit', function(e) {
-    e.preventDefault();
-
-    alert('Message sent successfully!');
-
-    form.reset();
-});
